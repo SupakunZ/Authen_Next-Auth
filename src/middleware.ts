0a8350a84693dd6b3cbcb9a986c36b2263035a18
Supakun Thata <supakunt.thata@gmail.com>
2024-10-12 12:17:07 +0700

    '#'

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,27 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = async (req: NextRequest) => {
   const { pathname, origin } = req.nextUrl;
+  //pathname -> params หน้านั้น
+  //origin -> หน้า /
+  //get token
   const session = await getToken({
     req
   })
+  console.log('dsdsd', pathname)
   //ตรวจสอบว่ามี session ไหมเมื่อเข้ามาหน้าแรก
   if (pathname == "/") {
-    if (!session) {
+    if (!session) { //ถ้าไม่มี token มาหน้า /auth
       return NextResponse.redirect(`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/auth`)
     }
-    if (pathname == "/auth") {
+    if (pathname == "/auth") { //ถ้ามี token มาหน้า /
       if (session) {
+        return NextResponse.redirect(`${origin}`)
+      }
+    }
+
+    //สมมุติว่ามี role admin ใช้ในการแยกสิทธิ
+    if (pathname.startsWith == "/admin") { //ถ้ามี token มาหน้า /
+      if (session.role === 'admin') { //ถ้ามี role admin อนุญาติ ให้ไปหน้า นี้ได้
         return NextResponse.redirect(`${origin}`)
       }
     }
